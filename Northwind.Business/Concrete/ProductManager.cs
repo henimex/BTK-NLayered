35a4fcc12135977ae5e6ec5fae5a29751dfae6f0
henimex <49072861+henimex@users.noreply.github.com>
2021-01-27 21:23:36 +0300

    Add project files.

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.DataAccess.Concrete;
+using Northwind.Entities.Concrete;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        public List<Product> GetAll()
+        {
+            ProductDal _productDal = new ProductDal();
+            return _productDal.GetAll();
+        }
+    }
+}
